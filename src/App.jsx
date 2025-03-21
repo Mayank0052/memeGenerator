4b12f83c53c26012073ef73d72fe5a17154ec5da
Mayank0052 <ms6065879@gmail.com>
2025-03-21 13:07:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx"
+import Main from "./components/Main.jsx"; // Correct path and file name
+
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
